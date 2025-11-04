b28162c405dc01671532bc85bf91ed1bf385627f
shreyaszcoer-jpg <shreyaszcoer@gmail.com>
2025-11-04 18:34:57 +0530

    Add files via upload

---
+++ api.d.ts
@@ -0,0 +1,48 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as ai from "../ai.js";
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
+import type * as outfits from "../outfits.js";
+import type * as profiles from "../profiles.js";
+import type * as rentals from "../rentals.js";
+import type * as router from "../router.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  ai: typeof ai;
+  auth: typeof auth;
+  http: typeof http;
+  outfits: typeof outfits;
+  profiles: typeof profiles;
+  rentals: typeof rentals;
+  router: typeof router;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
